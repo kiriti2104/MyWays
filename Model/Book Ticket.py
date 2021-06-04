c42f170ec9da7b646080d179532f72a5b9572a45
Kiriti Sharma <62835553+kiriti2104@users.noreply.github.com>
2021-06-04 14:32:18 +0530

    Add files via upload

---
+++ Model/Book Ticket.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Book Ticket:
+    def __init__(self):
+        self.NoOfTickets = None
+        self.Flightname = None
+        self.FlightNo = None
+        self.Date = None
+        self.Time = None
+        self.Airport = None
+
+    def UpdateSeatsAvailable(self, ):
+        pass
+
