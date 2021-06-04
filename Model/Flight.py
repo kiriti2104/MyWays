c50c28c44074ed12f6ace5eb2b9bbcbd3cf60229
Kiriti Sharma <62835553+kiriti2104@users.noreply.github.com>
2021-06-04 14:26:11 +0530

    Add files via upload

---
+++ Model/Flight.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Flight:
+    def __init__(self):
+        self.Flightname = None
+        self.Flighttime = None
+        self.Airport = None
+
+    def UpdateDetails(self, ):
+        pass
+
